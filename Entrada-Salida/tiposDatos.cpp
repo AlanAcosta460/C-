efdb581f7d28a64e652467207f816ce7f26b50c8
Alan Acosta <alanomar460@gmail.com>
2022-12-10 19:47:05 -0600

    tipos de datos basicos

---
+++ Entrada-Salida/tiposDatos.cpp
@@ -0,0 +1,19 @@
+//Tipos de datos básicos de C++
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int entero  = 27; //dato de tipo entero //= es un operador de asignacion
+    float flotante = 10.45; //dato de tipo flotante
+    double mayor = 3.14159265358979; //dato de tipo double, guarda numeros mas largos
+    char letra = 'a'; //dato tipo caracter //para asignarlo se usan las comillas simples ''
+
+    cout<<entero<<endl; //imprime la variable que se escriba despues de <<, no se pone entre ""
+    cout<<flotante<<endl; 
+    cout<<mayor<<endl;
+    cout<<letra<<endl;
+
+    return 0;
+}
