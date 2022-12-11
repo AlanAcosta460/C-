ac3dc9e6ce113cbd4c97a5d3278087c2a02d122b
Alan Acosta <alanomar460@gmail.com>
2022-12-10 20:21:01 -0600

    intro

---
+++ conceptosBasicos/intro.cpp
@@ -0,0 +1,11 @@
+//Librearia principal y primera impresion
+#include <iostream> //libreria de entrada y salida
+
+using namespace std; //para ahorarse el std::
+
+int main() //funcion principal
+{
+    cout<<"Hola mundo"<<endl; //cout<< imprime todo lo que este entre "" //<<endl tiene la misma funcion que \n
+
+    return 0;
+}
