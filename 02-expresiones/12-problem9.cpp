319085c6c66518416b448dbc0c2cd9877cf0f220
Alan Acosta <alanomar460@gmail.com>
2022-12-19 18:19:45 -0600

    promedio de 3 notas

--- 02-expresiones/12-problem9.cpp
+++ 02-expresiones/12-problem9.cpp
@@ -7,9 +7,9 @@ int main()
 {
     float nota1, nota2, nota3, promedio;
 
-    cout << "Ingrese la nota 1:  "; cin >> nota1;
-    cout << "Ingrese la nota 2:  "; cin >> nota2;
-    cout << "Ingrese la nota 3:  "; cin >> nota3;
+    cout << "Ingrese la nota 1: "; cin >> nota1;
+    cout << "Ingrese la nota 2: "; cin >> nota2;
+    cout << "Ingrese la nota 3: "; cin >> nota3;
 
     cout.precision(2);
     promedio = (nota1 + nota2 + nota3) / 3;
