0d15f682ebffc50f2ec98ad0906cd99722d6b39d
Alan Acosta <alanomar460@gmail.com>
2022-12-19 22:30:28 -0600

    problema 15, mayor de 3 numeros

---
+++ 03-condicionales/20-problem15.cpp
@@ -0,0 +1,24 @@
+//15. Escriba un programa que lea 3 números y determine cual de ellos es mayor
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float n1, n2, n3;
+
+    cout << "introduzca 3 numeros:\n"; cin >> n1 >> n2 >> n3;
+
+    if(n1 == n2 && n2 == n3)
+        cout << "\nLos tres numeros son iguales";
+    else
+        if(n1 > n2 && n1 > n3)
+            cout << "\nEl numero mayor es: " << n1 << endl;
+        else
+            if(n2 > n1 && n2 > n3)
+                cout << "\nEl numero mayor es: " << n2 << endl;
+            else
+                cout << "\nEl numero mayor es: " << n3 << endl;
+
+    return 0;
+}
