b7dba4bbf86bb780daaa271a16fe979cae15bd53
Alan Acosta <alanomar460@gmail.com>
2022-12-25 14:07:17 -0600

    mostrar la parte decimal

---
+++ 11-funciones/107-problem78.cpp
@@ -0,0 +1,36 @@
+/*78. Escriba un programa que devuelva la parte fraccionaria de cualquier 
+  número introducido por el usuario. Por ejemplo, si se introduce el número 
+  256.879, debería desplegarse el número 0.879
+*/
+
+#include <iostream>
+using namespace std;
+
+void pedirDatos();
+float decimal(float n);
+
+float numero;
+
+int main()
+{
+    pedirDatos();
+    numero = decimal(numero);
+    cout << "La parte decimal es: " << numero << endl;
+
+    cout << endl; system("pause");
+}
+
+void pedirDatos()
+{
+    cout << "Ingrese un numero con decimal: "; cin >> numero;
+}
+
+float decimal(float n)
+{
+    int aux;
+
+    aux = n;
+    n -= aux;
+
+    return n;
+}
