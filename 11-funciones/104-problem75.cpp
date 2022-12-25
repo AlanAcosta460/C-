30175e4272c56c229bca6442625d47283eb286c6
Alan Acosta <alanomar460@gmail.com>
2022-12-24 21:02:19 -0600

    multiplicar 2 numeros y variables globales

---
+++ 11-funciones/104-problem75.cpp
@@ -0,0 +1,31 @@
+/*75. Escriba una funcion llamada mult() que acepte dos números en punto
+  flotante como paramétros, multiplique estos dos números y despliegue el resultado
+*/
+
+#include <iostream>
+using namespace std;
+
+void pedirDatos();
+void mult(float n1, float n2);
+
+float numero1, numero2;
+
+int main()
+{
+    pedirDatos();
+
+    mult(numero1, numero2);
+    
+    cout << endl; system("pause");
+    return 0;
+}
+
+void pedirDatos()
+{
+    cout << "Ingrese dos numero con decimal: " << endl; cin >> numero1 >> numero2;
+}
+
+void mult(float n1, float n2)
+{
+    cout << "\nLa multiplicacion es: " << n1 * n2 << endl;
+}
