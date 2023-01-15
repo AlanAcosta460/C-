8a7a3361d4c6ede7eb028beded4498444e51134c
Alan Acosta <alanomar460@gmail.com>
2023-01-15 00:46:33 -0600

    buscar elemento en la lista

---
+++ 15-listas/161-buscar.cpp
@@ -0,0 +1,138 @@
+/*Pasos para mostrar un elemento de una lista: 
+1. Crear un nuevo nodo (actual).
+2. Igualar ese nuevo nodo (actual) a lista. 
+3. Recorrer la lista.
+4. Determinar si el elemento existe o no en la lista
+*/
+
+#include <iostream>
+using namespace std;
+
+struct Nodo
+{
+    int dato;
+    Nodo *siguiente;
+};
+
+void insertarLista(Nodo *&, int);
+void mostrarLista(Nodo *);
+void buscarLista(Nodo *, int);
+
+int menu();
+
+int main()
+{
+    Nodo *lista = NULL;
+    int dato, op;
+    
+    do
+    {
+        op = menu();
+
+        switch(op)
+        {
+            case 1:
+                cout << "Ingrese un numero: "; cin >> dato;
+                insertarLista(lista, dato);
+                system("pause");
+                break;
+            
+            case 2:
+                mostrarLista(lista);
+                cout << endl << endl;
+                system("pause");
+                break;
+
+            case 3:
+                cout << "Ingrese el elemento a buscar: "; cin >> dato;
+                buscarLista(lista, dato);
+                cout << endl; 
+                system("pause");
+                break;   
+            
+            default:
+                cout << "Gracias por usar el programa!!!" << endl;
+                break; 
+        }
+    } while(op != 4);
+
+    cout << endl; system("pause");
+    return 0;
+}
+
+void insertarLista(Nodo *&lista, int n)
+{
+    Nodo *nuevoNodo = new Nodo();
+    nuevoNodo->dato = n;
+
+    Nodo *aux1 = lista, *aux2;
+    
+    while (aux1 != NULL && aux1->dato < n)
+    {
+        aux2 = aux1;
+        aux1 = aux1->siguiente;
+    }
+    
+    if (lista == aux1)
+        lista = nuevoNodo;
+    else
+        aux2->siguiente = nuevoNodo;
+    
+    nuevoNodo->siguiente = aux1;
+
+    cout << "\tElemento " << n << " insertado a la cola!!!" << endl << endl;
+}
+void mostrarLista(Nodo *lista)
+{
+    Nodo *actual = new Nodo();
+    actual = lista;
+
+    if(actual != NULL)
+        cout << ".:Elementos de la fila:." << endl;
+    else
+        cout << "Fila Vacia";
+
+    while (actual != NULL)
+    {
+        cout << actual->dato << "\t";
+        actual = actual->siguiente;
+    }
+}
+void buscarLista(Nodo *lista, int n)
+{
+    Nodo *actual = new Nodo();
+    actual = lista;
+    bool flag = false;
+
+    while (actual != NULL && actual->dato <= n)
+    {
+        if (actual->dato == n)
+            flag = true;
+        actual = actual->siguiente;
+    }
+    
+    if (flag == true)
+        cout << "\tSe encontro el elemento " << n << " en la lista!!!" << endl;
+    else
+        cout << "\tNO se encontro el elemento " << n << " en la lista :(" << endl;
+}
+
+int menu()
+{
+    int op;
+
+    system("cls");
+    cout << "\t.::Menu::." << endl;
+    cout << "1. Insertar un numero a la lista" << endl;
+    cout << "2. Mostrar los elementos de la lista" << endl;
+    cout << "3. Buscar elemento en la lista" << endl;
+    cout << "4. Salir" << endl << endl;
+    
+    do
+    {
+        cout << "Ingrese una opcion: "; cin >> op;
+        cout << endl;
+    } while(op < 1 || op > 4);
+
+    return op;
+}
