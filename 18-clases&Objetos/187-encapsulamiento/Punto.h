ca87e36212b6ab87dba6c45c47b0f2ad55e3cce3
Alan Acosta <alanomar460@gmail.com>
2023-01-19 14:17:09 -0600

    visibilidad de los miembros de la clase

--- 18-clases&Objetos/187-encapsulamiento/Punto.h
+++ 18-clases&Objetos/187-encapsulamiento/Punto.h
@@ -1,7 +1,7 @@
 class Punto 
 {
     //public: -> cualquier codigo fuente puede acceder y modificar los valores
-    //protectect: -> la misma clase o clases hijas pueden acceder a los miembros de esta clase
+    //protectect: -> la misma clase o clases hijas pueden acceder a los miembros de esta clase  
     private:
         int x, y;
 
