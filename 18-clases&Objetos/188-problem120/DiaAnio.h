57734f75b835ba302c62b471db8e749cb60d7160
Alan Acosta <alanomar460@gmail.com>
2023-01-19 15:20:41 -0600

    clase: dia y mes

---
+++ 18-clases&Objetos/188-problem120/DiaAnio.h
@@ -0,0 +1,15 @@
+class DiaAnio
+{
+    private: 
+        int dia, mes;
+
+    public:
+        DiaAnio(int _dia, int _mes)
+        {
+            dia = _dia;
+            mes = _mes;
+        }
+
+        bool igual(DiaAnio &d);
+        void visualizar();
+};
---
+++ 18-clases&Objetos/188-problem120/main.cpp
@@ -0,0 +1,40 @@
+/*120. Definir una clase DiaAnio con los tributos mes y día. Los métodos igual() y visualizar().
+  El mes se registra como un valor entero (1 -> Enero, 2 -> Febrero, etc.). El dia del mes se registra
+  en otra variable entera dia. Escribir un programa 
+  que compruebe si una fecha es su cumpleaños
+*/
+
+#include <iostream>
+#include "DiaAnio.h"
+using namespace std;
+
+int main()
+{
+    DiaAnio *hoy, *cumple;
+    int d, m;
+
+    cout << ".:Ingrese la fecha de hoy:." << endl;
+    cout << "Dia: "; cin >> d;
+    cout << "Mes (numero): "; cin >> m;
+
+    hoy = new DiaAnio(d, m);
+    
+    cout << "\n.:Ingrese la fecha de su cumpleanios:." << endl;
+    cout << "Dia: "; cin >> d;
+    cout << "Mes (numero): "; cin >> m;
+
+    cumple = new DiaAnio(d, m);
+
+    cout << "\n.:Fecha de hoy:." << endl;
+    hoy->visualizar();
+    cout << "\n.:Fecha de su cumpleanios:." << endl;
+    cumple->visualizar();
+
+    if(hoy->igual(*cumple))
+        cout << "\nFeliz Cumpleanios!!!" << endl;
+    else
+        cout << "\nQue tenga un buen dia :)" << endl;
+
+    cout << endl; system("pause");
+    return 0;
+}
